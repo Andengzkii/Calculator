877da3663a020ca3c7b30502c58107d622d6b302
Andengzkii <andreagelleee@gmail.com>
2024-01-31 12:46:38 +0800

    calculator

---
+++ app/src/main/java/dev/andeng/calculator/LoadingScr.java
@@ -0,0 +1,14 @@
+package dev.andeng.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoadingScr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_scr);
+    }
+}
